9a85ad9c39fe8d124e3470baf81007b0bec5998e
kkkovalchukkk <student.kovalchuk@yandex.ru>
2024-02-08 05:21:36 +0700

    add root element no-scroll

--- scripts/burger-menu.js
+++ scripts/burger-menu.js
@@ -4,8 +4,10 @@ const mobileMenuEl = document.querySelector('.mobile-menu');
 headerBurgerBtnEl.addEventListener('click', () => {
   if (mobileMenuEl.classList.contains('active')) {
     mobileMenuEl.classList.remove('active');
+    document.rootElement.classList.remove('active');
     document.body.classList.remove('no-scroll');
   } else {
+    document.rootElement.classList.add('active');
     mobileMenuEl.classList.add('active');
     document.body.classList.add('no-scroll');
   }
@@ -13,6 +15,7 @@ headerBurgerBtnEl.addEventListener('click', () => {
 
 mobileMenuEl.querySelectorAll('a').forEach((a) =>
   a.addEventListener('click', () => {
+    document.rootElement.classList.remove('active');
     mobileMenuEl.classList.remove('active');
     document.body.classList.remove('no-scroll');
   })
